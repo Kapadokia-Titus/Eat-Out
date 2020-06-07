f4bb31095ee801d738a4d8101ddc12e0ee050468
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-06-07 05:53:47 +0300

    less bugs

--- app/src/main/java/kapadokia/nyandoro/eatout/Signup.java
+++ app/src/main/java/kapadokia/nyandoro/eatout/Signup.java
@@ -3,15 +3,24 @@ package kapadokia.nyandoro.eatout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class Signup extends AppCompatActivity {
 
     private EditText username,phone,password;
-    
+    private Button regBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+
+        username = findViewById(R.id.username_reg);
+        phone = findViewById(R.id.phone_number_reg);
+        password = findViewById(R.id.password_reg);
+
+
+        regBtn = findViewById(R.id.register_page_btn);
     }
 }
